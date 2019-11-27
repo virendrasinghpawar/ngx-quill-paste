bff91f1a8b5369a508feb0daf71aeba65bd89bfc
Walkover <walkover@Walkovers-iMac.local>
2019-11-27 10:43:16 +0530

    code commit initial

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ngx-quill-paste
+
+
+Workaround for Quill editor
+
+Issue – in  Quill.js flickers when you paste.
+
+See github issue : https://github.com/quilljs/quill/issues/1374
+
+
+Usage
+
+# in your project root
+npm i ngx-quill-paste
+// in your app – import and register the module
+import PasteClipboard from 'ngx-quill-paste'
+
+Quill.register('modules/clipboard', PasteClipboard);
---
+++ main.js
@@ -0,0 +1,22 @@
+import Quill from 'quill';
+import Delta from 'quill-delta';
+
+const Clipboard = Quill.import('modules/clipboard');
+
+export default class PasteClipboard extends Clipboard {
+    onPaste (e) {
+        if (e.defaultPrevented || !this.quill.isEnabled()) return;
+        let range = this.quill.getSelection();
+        let delta = new Delta().retain(range.index);
+        if (e && e.clipboardData && e.clipboardData.types && e.clipboardData.getData) {
+            let text = (e.originalEvent || e).clipboardData.getData('text/plain');
+            let cleanedText = this.convert(text);
+            e.stopPropagation();
+            e.preventDefault();
+            delta = delta.concat(cleanedText).delete(range.length);
+            this.quill.updateContents(delta, Quill.sources.USER);
+            this.quill.setSelection(delta.length() - range.length, Quill.sources.SILENT);
+            return false;
+        }
+    }
+}
