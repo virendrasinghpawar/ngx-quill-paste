da1cc022965669384e7367c7bfb22aedeaae8f80
Walkover <walkover@Walkovers-iMac.local>
2019-11-29 17:41:42 +0530

    html paste issue fix

--- main.js
+++ main.js
@@ -5,27 +5,19 @@ const Clipboard = Quill.import('modules/clipboard');
 
 export default class PasteClipboard extends Clipboard {
     onPaste(e) {
-      // get current page offset before paste
-      const top = window.pageYOffset;
-      const left = window.pageXOffset;
-  
-      if (e.defaultPrevented || !this.quill.isEnabled()) return;
-      let range = this.quill.getSelection();
-      let delta = new Delta().retain(range.index);
-      this.container.style.top = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0).toString() + 'px';
-      this.container.focus();
-      setTimeout(() => {
-        this.quill.selection.update(Quill.sources.SILENT);
-        delta = delta.concat(this.convert()).delete(range.length);
-        this.quill.updateContents(delta, Quill.sources.USER);
-        this.quill.setSelection(delta.length() - range.length, Quill.sources.SILENT);
-        let bounds = this.quill.getBounds(delta.length() - range.length, Quill.sources.SILENT);
-        this.quill.scrollingContainer.scrollTop = bounds.top;
-        
-        // scroll window to previous position after paste
-        window.scrollTo({top, left});
-      }, 1);
+        if (e.defaultPrevented || !this.quill.isEnabled()) return;
+        let range = this.quill.getSelection();
+        let delta = new Delta().retain(range.index);
+        if (e && e.clipboardData && e.clipboardData.types && e.clipboardData.getData) {
+            let text = (e.originalEvent || e).clipboardData.getData('text/html');
+            let cleanedText = this.convert(text);
+            e.stopPropagation();
+            e.preventDefault();
+            delta = delta.concat(cleanedText).delete(range.length);
+            this.quill.updateContents(delta, Quill.sources.USER);
+            this.quill.setSelection(delta.length() - range.length, Quill.sources.SILENT);
+            return false;
+        }
     }
   }
   
-  Quill.register('modules/clipboard', PasteClipboard, true);
